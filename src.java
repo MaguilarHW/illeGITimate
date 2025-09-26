72d952251ce3877fa76acc8b8ef21b73076f570c
Miles Aguilar <maguilar1@hwemail.com>
2025-09-26 01:17:40 -0700

    2.10.3 Removed redundant files

--- src.java
+++ src.java
@@ -3,8 +3,7 @@ import java.io.File;
 public class src {
     public static void main(String[] args) throws Exception {
         IlleGITimate git = new IlleGITimate();
-        git.commitFile(new File("testImages/a.jpg"));
-        git.commitFile(new File("testTextFiles/a2.txt"));
+        git.clearRepository();
         // do things here :D
     }
 }
